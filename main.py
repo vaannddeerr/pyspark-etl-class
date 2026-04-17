361650677544f1d9d430fe029806305895e9542e
Vanderlei Sousa de Carvalho <wsc_2014@live.com>
2026-04-17 19:51:00 +0100

    :wrench:primeira subida:rocket:

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from config_class_api import PipelineApi
 
-file_path = 'dadosabertos.json'
+file_path = 'dadosabertos_v2.json'
 path = f'/Volumes/workspace/default/landing_zone/{file_path}'
 
 minha_api = PipelineApi(url='https://dadosabertos.camara.leg.br/api/v2/deputados?ordem=ASC&ordenarPor=nome')
