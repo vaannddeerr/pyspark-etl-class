95d7acb3ebba91dbf666468a244ed5f435caf74f
Vanderlei Sousa de Carvalho <wsc_2014@live.com>
2026-04-18 03:37:22 +0100

    :wrench:primeira subida:rocket:

--- etl_process.py
+++ etl_process.py
@@ -1,12 +1,11 @@
 from pyspark.sql import functions as F
-from pyspark.sql import dataframe
 from config_class_api import PipelineApi
 
 
 
 
 
-def etl(df:dataframe):
+def etl(df):
 
     df = df.withColumn('dados', F.explode(F.col('dados')))\
            .withColumn('id', F.col('dados.id'))\
