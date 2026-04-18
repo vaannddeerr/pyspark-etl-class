84587d96a50ea888e7563549c02f3f8c849963bb
Vanderlei Sousa de Carvalho <wsc_2014@live.com>
2026-04-18 04:49:26 +0100

    :wrench:primeira subida:rocket:

--- etl_process.py
+++ etl_process.py
@@ -20,5 +20,5 @@ def etl(df):
            .withColumn('uriPartido', F.col('dados.uriPartido'))\
            .withColumn('urlFoto', F.col('dados.urlFoto')).drop(F.col('dados'))
     
-    return df.display()
+    return df.show(truncate=True)
 
