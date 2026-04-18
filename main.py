b6e17bed8b60bae2165d02f193898d1738cc1c0a
Vanderlei Sousa de Carvalho <wsc_2014@live.com>
2026-04-18 04:27:38 +0100

    :wrench:primeira subida:rocket:

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def executa_pipeline():
     resultado = menager.consummer_api()
     menager.save_response(path, resultado)
     menager.read_dataframe(path, is_path=False)
-    menager = etl(menager)
+    menager = etl(menager.df)
     
     return menager
 
