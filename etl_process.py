05b4df301191acf586751d185d9001f4adc02f45
Vanderlei Sousa de Carvalho <wsc_2014@live.com>
2026-04-18 05:50:12 +0100

    :wrench:primeira subida:rocket:

--- etl_process.py
+++ etl_process.py
@@ -25,7 +25,7 @@ def etl(df):
                F.col('idLegislatura')
               )
     print(f'💹volumetria:{df.count()}')
-    print(f'Aplicando transformação:')
+    print(f'🔃Aplicando transformação:')
 
     return df.show(truncate=True)
 
