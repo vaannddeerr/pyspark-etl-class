f7f33b006fd2bca98f09bd6a0fcb2e4969d0cd9f
Vanderlei Sousa de Carvalho <wsc_2014@live.com>
2026-04-18 05:18:49 +0100

    :wrench:primeira subida:rocket:

--- etl_process.py
+++ etl_process.py
@@ -24,6 +24,7 @@ def etl(df):
                F.col('email'),
                F.col('idLegislatura')
               )
+    print(f'Registros:{df.count()}')
     
     return df.show(truncate=True)
 
