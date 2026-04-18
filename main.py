68de45ae25bded2931ae01528d501594d6e53f07
Vanderlei Sousa de Carvalho <wsc_2014@live.com>
2026-04-18 05:28:46 +0100

    :wrench:primeira subida:rocket:

--- main.py
+++ main.py
@@ -5,18 +5,17 @@ from etl_process import etl
 def executa_pipeline():
     file_path = 'dadosabertos_v2.json'
     path = f'/Volumes/workspace/default/landing_zone/{file_path}'
-    print('Iniciando processamento...')
+    print('🔛Iniciando processamento.🔛')
     print('================================')
     menager = PipelineApi(url='https://dadosabertos.camara.leg.br/api/v2/deputados?ordem=ASC&ordenarPor=nome')
-    print(f'Consumindo API...')
+  
 
-    print('================================')
     resultado = menager.consummer_api()
-    print(f'Resultado da solicitação...')
+    print(f'📖Resultado da solicitação📖')
 
     print('================================')
     menager.save_response(path, resultado)
-    print(f'Salvando resultado...')
+    print(f'✔️Arquivo salvo com sucesso✔️')
 
     print('================================')
     menager.read_dataframe(path, is_path=True)
