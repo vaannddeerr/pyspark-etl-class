a46bb41314f18e305973d73e985e2a824852fadf
Vanderlei Sousa de Carvalho <wsc_2014@live.com>
2026-04-18 03:55:12 +0100

    :wrench:primeira subida:rocket:

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def executa_pipeline():
     menager = PipelineApi(url='https://dadosabertos.camara.leg.br/api/v2/deputados?ordem=ASC&ordenarPor=nome')
     resultado = menager.consummer_api()
     menager.save_response(path, resultado)
-    menager.read_dataframe(menager)
+    menager.read_dataframe(menager, is_path=False)
     
 
     
